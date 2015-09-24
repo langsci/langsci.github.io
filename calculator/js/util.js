84631b5bb1bd7c96415d97c13711ae84bf98182e
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2015-09-24 10:07:23 +0200

    test

--- calculator/js/util.js
+++ calculator/js/util.js
@@ -4,7 +4,7 @@
   //Create the SVG graph.
   var svgdiv = d3.select("body").append("div");
   svgdiv.attr("id","svgdiv");
-  var svg = svgdiv.append("svg").attr("width", "100%").attr("height", "100%");
+  var svg = svgdiv.append("svg").attr("width", "50%").attr("height", "50%");
    
    
    
