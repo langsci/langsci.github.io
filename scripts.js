67a1d090e88e2c681e3829981a99b7d1b72c2917
Schmalhans <lqfb@glottotopia.de>
2016-07-04 10:20:33 +0200

    forked

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 // Your scripts go here.
 
 console.log('hello world!');
+// alert(123)
+  
+  $(document).ready(function() 
+  { 
+//     alert(234)
+    $("#myTable").tablesorter(); 
+//     alert(5)
+  } 
+  ); 
+  
