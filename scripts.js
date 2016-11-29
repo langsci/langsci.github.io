ba3d8b8137ca02d22da53a37635d7b9523b4b450
Sebastian Nordhoff <sebastian.nordhoff@glottotopia.de>
2016-11-29 16:32:20 +0100

    yaml for sliders

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() 
   {  
+//     catalogtable
 //     $("#catalogtable").tablesorter();  
     $("#catalogtable").DataTable();  
+
+// ----------------------  
+//  slider 
+    $("#owl-slider").owlCarousel({
+	    navigation: true,
+	    pagination: true,
+	    navigationText: ['&#9664;','&#9654;'],
+	    singleItem: true,
+	    slideSpeed : 2000,
+	    rewindSpeed : 4000,
+	    autoHeight: true
+    });  
   } 
-  ); 
-  
+); 
+
+ 
